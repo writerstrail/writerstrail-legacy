0df4fdc149002a4e3dd692d29fc544a98f48ef33
George Marques <george@georgemarques.com.br>
2014-12-24 06:28:52 -0200

    Adding bower dependencies

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use('/bower_components',express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
