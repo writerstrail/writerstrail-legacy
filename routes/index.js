6bc7d5557bffdf48657e89492a776d75e53aac29
George Marques <george@georgemarques.com.br>
2014-12-24 07:38:35 -0200

    Changing home page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Writer\'s Trail', section:'home' });
+    res.render('index', { title: 'Writer\'s Charts', section:'home' });
 });
 
 module.exports = router;
