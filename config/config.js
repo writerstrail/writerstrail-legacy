42ab74e2e901c23ac81b43f6e9c2a7f8e8eae253
George Marques <george@georgemarques.com.br>
2014-12-26 21:37:19 -0200

    Adding sequelize ORM structure

---
+++ config/config.js
@@ -0,0 +1,25 @@
+module.exports =
+{
+  "development": {
+    "username": "nwt",
+    "password": "123123",
+    "database": "nwt",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    "password": process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    "database": "writerscharts",
+    "port": process.env.OPENSHIFT_MYSQL_DB_PORT,
+    "host": process.env.OPENSHIFT_MYSQL_DB_HOST,
+    "dialect": "mysql"
+  }
+};
