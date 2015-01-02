473d093cd870422c884947711b139ec3aacd63c2
George Marques <george@georgemarques.com.br>
2015-01-02 18:51:45 -0200

    Making user model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,59 @@
-﻿"use strict";
+"use strict";
 
 module.exports = function (sequelize, DataTypes) {
 	var User = sequelize.define("User", {
-		username: DataTypes.STRING
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		name: {
+			type: DataTypes.STRING
+		},
+		facebookId: {
+			type: DataTypes.STRING,
+			unique: true
+		},
+		facebookToken: {
+			type: DataTypes.STRING
+		},
+		facebookEmail: {
+			type: DataTypes.STRING
+		},
+		facebookName: {
+			type: DataTypes.STRING
+		},
+		twitterId: {
+			type: DataTypes.STRING,
+			unique: true
+		},
+		twitterToken: {
+			type: DataTypes.STRING					
+		},
+		twitterDisplayName: {
+			type: DataTypes.STRING
+		},
+		twitterUsername: {
+			type: DataTypes.STRING
+		},
+		googleId: {
+			type: DataTypes.STRING,
+			unique: true
+		},
+		googleToken: {
+			type: DataTypes.STRING
+		},
+		googleEmail: {
+			type: DataTypes.STRING
+		},
+		googleName: {
+			type: DataTypes.STRING
+		}
 	}, {
 		classMethods: {
-			associate: function (models) {
-				User.hasMany(models.Genre);
-			}
-		}
+		},
+		paranoid: true,
 	});
-
+	
 	return User;
 };
