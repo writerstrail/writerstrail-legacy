5ccad1418a1a1e41362fd9ca73423e1938f3a155
George Marques <george@georgemarques.com.br>
2015-01-03 14:32:30 -0200

    Minor layout fixes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Writer\'s Charts', section:'home' });
+    res.render('index', { section:'home' });
 });
 
 module.exports = router;
