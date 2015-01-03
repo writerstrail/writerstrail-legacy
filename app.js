b99ef0a4cf300251ef8258195474d56ae331c6f3
George Marques <george@georgemarques.com.br>
2015-01-03 13:58:14 -0200

    Setting default options to session

--- app.js
+++ app.js
@@ -30,7 +30,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: (process.env.WRITERSTRAIL_SESSION_SECRET || 'changemeasimnotsecret') }));
+app.use(session({
+	secret: (process.env.WRITERSTRAIL_SESSION_SECRET || 'changemeasimnotsecret'),
+	saveUninitialized: true,
+	resave: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
