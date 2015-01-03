074fd2716b6b6826aee55422eea60efd000bcf7a
George Marques <george@georgemarques.com.br>
2015-01-03 14:10:38 -0200

    Adding CSRF protection

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express'),
 	session = require('express-session'),
 	passport = require('passport'),
 	flash = require('connect-flash'),
-	i18n = require('i18n');
+	i18n = require('i18n'),
+	csrf = require('csurf');
 
 var app = express();
 
@@ -39,8 +40,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(i18n.init);
+app.use(csrf());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use('/bower_components',express.static(path.join(__dirname, 'bower_components')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var routes = require('./routes/index');
@@ -50,14 +52,20 @@ var navlist = require('./routes/navlist.js');
 // Adding navlist
 app.locals.navlist = navlist;
 
+// Adding csrf token
+
+app.use(function (req, res, next) { 
+	res.locals.csrf = req.csrfToken();
+});
+
 app.use('/', routes);
 app.use('/', authRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -65,23 +73,23 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
