d4c3b936afdf50edbe5e7f83386e0f15d47009c8
George Marques <george@georgemarques.com.br>
2015-01-08 20:10:02 -0200

    Allowing user selection by clicking on table row

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -4,21 +4,24 @@
 	$("#select_all").click(function () {
 		var el = $(this);
 		var selection = $('.selection');
-    
+		var tr = $('#users .tr')
+		
 		if (this.checked) {
 			selection.prop('checked', true);
+			tr.addClass('success');
 		} else {
 			selection.prop('checked', false);
+			tr.removeClass('success');
 		}
-
+		
 		el.prop('indeterminate', false);
 	});
-
-	$(".selection").click(function () {
+	
+	$(".selection").change(function () {
 		var selection = $('.selection:checked');
 		var all = $('.selection');
 		var el = $('#select_all');
-
+		
 		if (selection.length === 0) {
 			el.prop('checked', 0);
 			el.prop('indeterminate', false);
@@ -28,6 +31,16 @@
 		} else {
 			el.prop('checked', 1);
 			el.prop('indeterminate', false);
+		}
+		if (this.checked) $(this).closest('tr').addClass('success');
+		else $(this).closest('tr').removeClass('success');
+	});
+	
+	$("#users tr").click(function (event) {
+		if (event.target.type !== 'checkbox' &&
+				event.target.nodeName !== 'BUTTON' &&
+				event.target.nodeName !== 'A') {
+			$(':checkbox', this).trigger('click');
 		}
 	});
 })(jQuery);
