44c018b40ec8d2c106ac4a5ca21e58e349ee9799
George Marques <george@georgemarques.com.br>
2015-01-16 15:58:20 -0200

    Adding paginate middleware

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express'),
-	path = require('path'),
-	favicon = require('serve-favicon'),
-	logger = require('morgan'),
-	cookieParser = require('cookie-parser'),
-	bodyParser = require('body-parser'),
-	session = require('express-session'),
-	MysqlStore = require('express-mysql-session'),
-	passport = require('passport'),
-	flash = require('connect-flash'),
-	i18n = require('i18n'),
-	csrf = require('csurf'),
-	moment = require('moment'),
-	_ = require('lodash');
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  MysqlStore = require('express-mysql-session'),
+  passport = require('passport'),
+  flash = require('connect-flash'),
+  i18n = require('i18n'),
+  csrf = require('csurf'),
+  paginate = require('express-paginate'),
+  moment = require('moment'),
+  _ = require('lodash');
 
 var app = express();
 
@@ -20,11 +21,11 @@ var config = require('./config/config.js')[env];
 
 // create session store
 var sessionStore = new MysqlStore({
-	host: config.host,
-	port: config.port || 3306,
-	user: config.username,
-	password: config.password,
-	database: config.database
+  host: config.host,
+  port: config.port || 3306,
+  user: config.username,
+  password: config.password,
+  database: config.database
 });
 
 // view engine setup
@@ -33,8 +34,8 @@ app.set('view engine', 'jade');
 
 // i18n configuration
 i18n.configure({
-	locales: ['en', 'pt'],
-	directory: path.join(__dirname, 'locales')
+  locales: ['en', 'pt'],
+  directory: path.join(__dirname, 'locales')
 });
 
 // passport configuration
@@ -47,10 +48,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-	secret: (process.env.WRITERSTRAIL_SESSION_SECRET || 'changemeasimnotsecret'),
-	store: sessionStore,
-	saveUninitialized: true,
-	resave: true
+  secret: (process.env.WRITERSTRAIL_SESSION_SECRET || 'changemeasimnotsecret'),
+  store: sessionStore,
+  saveUninitialized: true,
+  resave: true
 }));
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
@@ -60,6 +61,7 @@ app.use(i18n.init);
 app.use(csrf());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(paginate.middleware(10, 50));
 
 var routes = require('./routes/index');
 var authRoutes = require('./routes/auth')(passport);
@@ -69,15 +71,15 @@ var navlist = require('./routes/navlist');
 
 // Adding locals
 app.use(function (req, res, next) {
-	res.locals.csrf = req.csrfToken();
-	if (req.isAuthenticated()) {
-		res.locals.user = req.user;
-	}
-	res.locals.navlist = navlist(req);
-	
-	res.locals._ = _;
-	res.locals.moment = moment;
-	next();
+  res.locals.csrf = req.csrfToken();
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  res.locals.navlist = navlist(req);
+
+  res.locals._ = _;
+  res.locals.moment = moment;
+  next();
 });
 
 app.use('/', routes);
@@ -87,9 +89,9 @@ app.use('/', userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -97,44 +99,43 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function (err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
 // 404 error handler
 app.use(function (err, req, res, next) {
-	if (!(404 === err.status)) return next(err);
-	
-	res.render('error/404', {
-		title: req.__('Page not found - 404'),
-		section: '404'
-	});
+  if (!(404 === err.status)) { return next(err); }
+
+  res.render('error/404', {
+    title: req.__('Page not found - 404'),
+    section: '404'
+  });
 });
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-	res.status(err.status || 500);
-	
-	res.locals.csrf = req.csrfToken();
-	if (req.isAuthenticated()) {
-		res.locals.user = req.user;
-	}
-	res.locals.navlist = navlist(req);
-	
-	res.locals._ = _;
-	res.locals.moment = moment;
-	
-	res.render('error/500', {
-		title: req.__('Server error - 500'),
-		section: '505'
-	});
-});
+  res.status(err.status || 500);
 
+  res.locals.csrf = req.csrfToken();
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  res.locals.navlist = navlist(req);
+
+  res.locals._ = _;
+  res.locals.moment = moment;
+
+  res.render('error/500', {
+    title: req.__('Server error - 500'),
+    section: '505'
+  });
+});
 
 module.exports = app;
