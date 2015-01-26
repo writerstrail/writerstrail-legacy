bad9252898633cfb2316e7694802096051f8f945
George Marques <george@georgemarques.com.br>
2015-01-25 22:02:11 -0200

    Adding more info to dashboard stats

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var router = require('express').Router(),
   promise = require('sequelize').Promise,
-  moment = require('moment'),
+  _ = require('lodash'),
+  //moment = require('moment'),
   genres = require('./users/genres'),
   projects = require('./users/projects'),
   targets = require('./users/targets'),
@@ -8,6 +9,13 @@ var router = require('express').Router(),
   settings = require('./users/settings'),
   isactivated = require('../utils/middlewares/isactivated'),
   models = require('../models');
+
+function durationFormatterAlt(dur) {
+  var min = Math.floor(dur / 60),
+    sec = dur - min * 60;
+  
+  return (min.toString() +  'm' + (sec < 10 ? '0' + sec : sec)) + 's';
+}
 
 router.use(isactivated);
 
@@ -29,7 +37,6 @@ router.use(sessions);
 router.use('/settings', settings);
 
 router.get('/dashboard', function (req, res, next) {
-  
   var getProjects = function () {  
       return models.Project.findAll({
         where: {
@@ -53,7 +60,7 @@ router.get('/dashboard', function (req, res, next) {
         where: [
           { ownerId: req.user.id },
           models.Sequelize.literal(
-              'CASE WHEN `Target`.`zoneOffset` IS NOT NULL THEN `Target`.`end` >= DATE_SUB(NOW(), INTERVAL `Target`.`zoneOffset` MINUTE)' +
+              'CASE WHEN `Target`.`zoneOffset` IS NOT NULL THEN `Target`.`end` >= DATE_SUB(NOW(), INTERVAL `Target`.`zoneOffset` MINUTE) ' +
               'ELSE `Target`.`end` >= NOW() END')
         ],
         order: [['end', 'ASC']]
@@ -68,18 +75,50 @@ router.get('/dashboard', function (req, res, next) {
         }
       });
     },
-    renderer = function (projects, target, totalWordcount) {
+    getDailyCounts = function () {
+      return models.Session.findAll({
+        attributes: [
+          models.Sequelize.literal('SUM(`Session`.`wordcount`) AS `dailyCount`'),
+          models.Sequelize.literal('(SUM(`Session`.`duration`) / 60) AS `totalDuration`'),
+          models.Sequelize.literal('DATE(`Session`.`start`) AS `date`')
+        ],
+        include: [{
+          model: models.Project,
+          as: 'project',
+          where: {
+            ownerId: req.user.id
+          }
+        }],
+        group: [models.Sequelize.literal('DATE(`Session`.`start`)')]
+      }, {
+        raw: true
+      });
+    },
+    getPerformancePeriod = function () {
+      return models.sequelize.query("SELECT AVG(s.wordcount / (s.duration / 60)) AS performance, CASE WHEN TIME(start) BETWEEN '06:00:00' AND '11:59:59' THEN 'morning' WHEN TIME(start) BETWEEN '12:00:00' AND '17:59:59' THEN 'afternoon' WHEN TIME(start) BETWEEN '18:00:00' AND '23:59:59' THEN 'evening' WHEN TIME(start) BETWEEN '00:00:00' AND '5:59:59' THEN 'early morning' END AS period FROM writingSessions s INNER JOIN projects p ON p.id = s.projectId AND p.ownerId = " + req.user.id + " GROUP BY period ORDER BY performance DESC LIMIT 1");
+    },
+    getPerformanceSession = function () {
+      return models.sequelize.query("SELECT AVG(s.wordcount / (s.duration / 60)) AS performance, CASE WHEN isCountdown = 0 THEN 'forward' ELSE 'countdown' END AS direction, duration FROM writingSessions s INNER JOIN projects p ON p.id = s.projectId AND p.ownerId = " + req.user.id + " GROUP BY duration, isCountdown ORDER BY performance DESC LIMIT 1;");
+    },
+    renderer = function (projects, target, totalWordcount, dailyCounts, perfPeriod, perfSession) {
+      var totalDailyCounts = _.reduce(dailyCounts, function (acc, d) { return acc + d.dailyCount; }, 0),
+        totalDuration = _.reduce(dailyCounts, function (acc, d) { return acc + d.totalDuration; }, 0);
       res.render('user/dashboard', {
         title: 'Dashboard',
         section: 'dashboard',
         projects: projects,
         target: target,
         stats: {
-          totalWordcount: totalWordcount
-        }
+          totalWordcount: totalWordcount,
+          dailyAverage: dailyCounts.length > 0 ? totalDailyCounts / dailyCounts.length : 0,
+          wpm: dailyCounts.length > 0 ? totalDailyCounts / totalDuration : 0,
+          period: perfPeriod.length > 0 ? perfPeriod[0] : null,
+          session: perfSession.length > 0 ? perfSession[0] : null
+        },
+        durationFormatter: durationFormatterAlt
       });
     };
-  promise.join(getProjects(), getTarget(), getTotalWordcount(), renderer)
+  promise.join(getProjects(), getTarget(), getTotalWordcount(), getDailyCounts(), getPerformancePeriod(), getPerformanceSession(), renderer)
   .catch(function (err) {
     next(err);
   });
