52934820d09170f6893ff29caf6ee14f2eacc740
George Marques <george@georgemarques.com.br>
2015-02-02 20:37:14 -0200

    Enabling feedback creation

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,9 +2,10 @@ var router = require('express').Router(),
   promise = require('sequelize').Promise,
   islogged = require('../utils/middlewares/islogged'),
   isactivated = require('../utils/middlewares/isactivated'),
+  sendflash = require('../utils/middlewares/sendflash'),
   models = require('../models');
 
-router.get('/', function (req, res, next) {
+router.get('/', sendflash, function (req, res, next) {
   var userVotes = null;
   (function () {
     if (!req.user) {
@@ -21,7 +22,7 @@ router.get('/', function (req, res, next) {
       include: {
         model: models.Vote,
         as: 'votes',
-        required: true
+        required: false
       },
       attributes: [
         'id',
@@ -31,6 +32,7 @@ router.get('/', function (req, res, next) {
       ],
       group: 'Feedback.id',
       order: [models.Sequelize.literal('totalVotes DESC')]
+    
     }, {
       raw: true
     });
@@ -46,11 +48,40 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/new', isactivated, function (req, res) {
+router.get('/new', isactivated, sendflash, function (req, res) {
+  var validate = req.flash('valerror');
+  var values = req.flash('values');
   res.render('feedback/form', {
     title: 'New feedback',
     section: 'feedbackform',
-    types: ['Bug', 'Suggestion', 'Feedback']
+    types: ['Bug', 'Suggestion', 'Feedback'],
+    validate: validate.length > 0 ? validate[0].errors : [],
+    feedback: values.length > 0 ? values[0] : {}
+  });
+});
+
+router.post('/new', isactivated, function (req, res, next) {
+  models.Feedback.create({
+    authorId: req.user.id,
+    summary: req.body.summary,
+    description: req.body.description,
+    type: req.body.type
+  }).then(function () {
+    req.flash('success', 'The feedback "' + req.body.summary + '" was successfully created.');
+    return res.redirect('/feedback/mine');
+  }).catch(function (err) {
+    if (err.name === 'SequelizeValidationError') {
+      req.flash('error', 'There are invalid values');
+      req.flash('valerror', err);
+      req.flash('values', {
+        summary: req.body.summary,
+        description: req.body.description,
+        type: req.body.type
+      });
+      res.redirect('back');
+    } else {
+      next(err);
+    }
   });
 });
 
