46098e3b0886ba46d5f8807ee269ca886e905cbf
George Marques <george@georgemarques.com.br>
2015-02-03 20:15:27 -0200

    Allowing to restore feedbacks

--- routes/admin/feedback.js
+++ routes/admin/feedback.js
@@ -165,13 +165,29 @@ router.get('/:id/delete', sendflash, function (req, res, next) {
       id: req.params.id
     }
   }).then(function (rows) {
-    console.log('------rows', rows);
     if (rows > 0) {
       req.flash('success', 'Feedback successfully deleted.');
     } else {
       req.flash('warning', 'No feedback to delete');
     }
     res.redirect('/admin/feedback');
+  }).catch(function (err) {
+    next(err);
+  });
+});
+
+router.get('/:id/delete/undo', sendflash, function (req, res, next) {
+  models.Feedback.restore({
+    where: {
+      id: req.params.id
+    }
+  }).then(function (rows) {
+    if (rows) {
+      req.flash('success', 'Feedback successfully restored.');
+    } else {
+      req.flash('warning', 'No feedback to restore');
+    }
+    res.redirect('/admin/feedback/' + req.params.id);
   }).catch(function (err) {
     next(err);
   });
