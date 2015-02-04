6f9d4a3728362527e9f09ce785323375a79c811b
George Marques <george@georgemarques.com.br>
2015-02-03 22:26:35 -0200

    Better usability

--- routes/feedback.js
+++ routes/feedback.js
@@ -179,6 +179,7 @@ router.get('/:id', sendflash, function (req, res, next) {
         'summary',
         'type',
         'authorId',
+        'originalAuthorId',
         'deletedAt',
         'status',
         'response',
