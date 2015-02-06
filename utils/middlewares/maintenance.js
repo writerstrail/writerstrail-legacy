6b208283f9e21b5fbbd83382841fff1b515f3c2c
George Marques <george@georgemarques.com.br>
2015-02-06 13:11:09 -0200

    Including maintenance features

---
+++ utils/middlewares/maintenance.js
@@ -0,0 +1,38 @@
+// Maintenance middleware
+var http = require('request'),
+  router = require('express').Router(),
+  models = require('../../models'),
+  _ = require('lodash'),
+  allowedUrls= ['/', '/features', '/tos', '/privacy', '/donations'];
+
+router.use(function maintenance(req, res, next) {
+  models.App.findOne(1).then(function (app) {
+    if (app.maintenance === 'soft') {
+      req.flash('maintenance', "Writer's Trail is under maintenance. You may experience some issues.");
+      return next();
+    } else if (app.maintenance === 'hard') {
+      if (req.query.maintenance === process.env.WRITERSTRAIL_MAINTENANCE_KEY) {
+        return next();
+      } else {
+        req.logout();
+        if (_.some(allowedUrls, function (i) {
+          return req.originalUrl === i;
+        })) {
+          return next();
+        }
+        return http.get('http://thecatapi.com/api/images/get?format=html&type=jpg,png&size=med', function (err, resp, cat) {
+          console.log('----cat', cat);
+          return res.render('error/maintenance', {
+            cat: cat
+          });
+        });
+      }
+    } else {
+      return next();
+    }
+  }).catch(function () {
+    next();
+  });
+});
+
+module.exports = router;
