a5861f3b5bb02264ed7aa74f2a2a4411a063bc8f
George Marques <george@georgemarques.com.br>
2015-02-06 12:21:48 -0200

    Adding application config resource

--- migrations/next.js
+++ migrations/next.js
@@ -2,10 +2,40 @@
 
 module.exports = {
   up: function (migration, DataTypes, done) {
-    migration.renameColumn('settings', 'showPondered', 'showAdjusted').then(done);
+    migration.renameColumn('settings', 'showPondered', 'showAdjusted').then(function () {
+      return migration.createTable('application', {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: false,
+          primaryKey: true
+        },
+        maintenance: {
+          type: DataTypes.ENUM,
+          values: ['off', 'soft', 'hard'],
+          allowNull: false,
+          defaultValue: 'off',
+          comment: "Enable/disable maintenance mode"
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        }
+      },
+      {
+        charset: 'utf8'
+      });
+    }).then(function () {
+      return migration.sequelize.query("INSERT INTO application (id, maintenance, createdAt, updatedAt) VALUES (1, 'off', NOW(), NOW());");
+    }).then(done);
   },
   
   down: function (migration, DataTypes, done) {
-    migration.renameColumn('settings', 'showAdjusted', 'showPondered').then(done);
+    migration.dropTable('application').then(function () {
+      return migration.renameColumn('settings', 'showAdjusted', 'showPondered');
+    }).then(done);
   }
 };
