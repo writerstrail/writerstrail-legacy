e8bb167d31aef093d9b525ae7f74149f885b1eb4
George Marques <george@georgemarques.com.br>
2015-02-10 15:46:19 -0200

    Functional away button

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -34,31 +34,45 @@ function timerSetup($, c3) {
     options.donut.title = 'Seconds';
     options.bindto = '#timer-sec';
     var timerSec = c3.generate(options);
+    $('#timerpause').data('away', false).data('time', 0).click(function () {
+      var self = $(this);
+      if ($('#timerstart').data('running')) {
+        if (self.data('away')) {
+          self.data('away', false).html("I'm away");
+        } else {
+          self.data('away', true).html("I'm back");
+        }
+      }
+    });
     
     var onstop = function (){
       var self = $('#timerstart');
       clearInterval(self.data('interval'));
       self.html('Start').removeClass('btn-danger').addClass('btn-primary');
       self.data('running', false);
-      $('#timerpause').html("I'm away").prop('disabled', true);
+      $('#timerpause').html("I'm away").data('away', false).prop('disabled', true);
     };
     
     $('#timer-min').data('c3', timerMin);
     $("#timerstart").data('running', false).click(function () {
       var self = $(this);
+      var pause = $('#timerpause');
       
       if (self.data('running')) {
         onstop();
       } else {
         self.html('Stop').removeClass('btn-primary').addClass('btn-danger');
         self.data('running', true);
-        $('#timerpause').prop('disabled', false);
+        pause.html("I'm away").data('time', 0).prop('disabled', false);
 
         var minutes = Math.max(0, parseInt($('#min').val(), 10));
         var seconds = Math.max(0, parseInt($('#sec').val(), 10));
         
         var loop = function () {
             self.data('seconds', self.data('seconds') - 1);
+            if (pause.data('away')) {
+              pause.data('time', pause.data('time') + 1);
+            }
             if (self.data('seconds') < 0) {
               self.data('seconds', 59);
               self.data('minutes', self.data('minutes') - 1);
