15a043af3ea79a1eb0bd3192f0d5dfe3744bd256
George Marques <george@georgemarques.com.br>
2015-02-18 19:30:31 -0200

    Changing message for not activated

--- utils/middlewares/isactivated.js
+++ utils/middlewares/isactivated.js
@@ -5,7 +5,7 @@ module.exports = function isactivated(req, res, next) {
   } else if (req.user.password === null) {
     res.redirect('/password/create');
   } else if (!req.user.activated) {
-    req.flash('error', req.__('Sorry, your account is still not activated.'));
+    req.flash('error', req.__('Sorry, your account is still not activated. You can activate your account now if you have an invitation code for beta.'));
     res.redirect('/account');
   } else {
     next();
