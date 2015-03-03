f2ed69244f9dc88b97965195bd5c51a764a94fe4
George Marques <george@georgemarques.com.br>
2015-03-03 11:01:19 -0300

    Be more verbose about database operations in tests

--- test/init.js
+++ test/init.js
@@ -3,10 +3,18 @@ var exec = require('child_process').exec;
 
 before(function buildDb(done) {
   this.timeout(20000);
-  exec('make migrate NODE_ENV=' + process.env.NODE_ENV, done);
+  console.log('Building database...');
+  exec('make migrate NODE_ENV=' + process.env.NODE_ENV, function () {
+    console.log('Database built.');
+    done();
+  });
 });
 
 after(function dropDb(done) {
   this.timeout(20000);
-  exec('make undomigrate NODE_ENV=' + process.env.NODE_ENV, done);
+  console.log('Dropping database...');
+  exec('make undomigrate NODE_ENV=' + process.env.NODE_ENV, function () {
+    console.log('Database dropped.');
+    done();
+  });
 });
