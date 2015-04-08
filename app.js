c8d9a0ac2514bec14cf39f04dc93781d63a71774
George Marques <george@georgemarques.com.br>
2015-04-08 10:49:28 -0300

    Fix uglify middleware to avoid non-proper caching

--- app.js
+++ app.js
@@ -74,10 +74,10 @@ app.use(session({
 }));
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(uglify(path.join(__dirname, 'public'), {
   generateSourceMap: env !== 'production'
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 app.use(i18n.init);
 app.use(csrf());
