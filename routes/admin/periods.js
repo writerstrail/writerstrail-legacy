5c3865d0e3ef67e829a4645ec67f4317ed818510
George Marques <george@georgemarques.com.br>
2015-04-25 21:17:59 -0300

    Show periods list

--- routes/admin/periods.js
+++ routes/admin/periods.js
@@ -1,11 +1,19 @@
 var router = require('express').Router(),
   models = require('../../models');
 
-router.get('/', function (req, res) {
-  res.render('admin/periods/index', {
-    title: 'Periods',
-    section: 'adminperiods'
-  });
+router.get('/', function (req, res, next) {
+  models.sequelize.query('SELECT * FROM `periods` ORDER BY `start` ASC;')
+    .then(function (periods) {
+      console.log(periods);
+      res.render('admin/periods/index', {
+        title: 'Periods',
+        section: 'adminperiods',
+        periods: periods
+      });
+    })
+    .catch(function (err) {
+      next(err);
+    });
 });
 
 module.exports = router;
